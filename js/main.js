cf4d39301bd759e4958cc347901f66d509cdc96e
judehouse <87717887+judehouse@users.noreply.github.com>
2021-07-29 14:13:45 -0400

    🎉added mobile menu🎉

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const mobileTrigger = document.querySelector(".header-nav-trigger");
+const mobileTrigger2 = document.querySelector(".mobile-menu-exit");
 const body = document.body;
 mobileTrigger.addEventListener("click", function () {
+  body.classList.toggle("menu-active");
+});
+
+mobileTrigger2.addEventListener("click", function () {
   body.classList.toggle("menu-active");
 });
